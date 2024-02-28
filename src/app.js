3785ec03c88fe5bec073d0e3f8b1c59ff389fb78
Rahul <rahul@Rahuls-MacBook-Air.local>
2024-02-28 10:33:26 +0530

    completed

--- src/app.js
+++ src/app.js
@@ -1,59 +1,82 @@
 const PLAYERS = [
-    "Spiderman",
-    "Captain America",
-    "Wonderwoman",
-    "Popcorn",
-    "Gemwoman",
-    "Bolt",
-    "Antwoman",
-    "Mask",
-    "Tiger",
-    "Captain",
-    "Catwoman",
-    "Fish",
-    "Hulk",
-    "Ninja",
-    "Black Cat",
-    "Volverine",
-    "Thor",
-    "Slayer",
-    "Vader",
-    "Slingo"
+  "Spiderman",
+  "Captain America",
+  "Wonderwoman",
+  "Popcorn",
+  "Gemwoman",
+  "Bolt",
+  "Antwoman",
+  "Mask",
+  "Tiger",
+  "Captain",
+  "Catwoman",
+  "Fish",
+  "Hulk",
+  "Ninja",
+  "Black Cat",
+  "Volverine",
+  "Thor",
+  "Slayer",
+  "Vader",
+  "Slingo",
 ];
 
-// initialize players with image and strength
+// Initialize players with image and strength
 const initPlayers = (players) => {
-    let detailedPlayers = '';
+  return players.map((e, index) => {
+    return {
+      name: e,
+      strength: getRandomStrength(),
+      image: "images/super-" + (index + 1) + ".png",
+      type: index % 2 === 0 ? "hero" : "villain",
+    };
+  });
+};
 
-    // Instead of forloop use Map method
-    // Code here
-
-    return detailedPlayers;
-}
-
-// getting random strength
+// Getting random strength
 const getRandomStrength = () => {
-    return Math.ceil(Math.random() * 100);
-}
+  return Math.ceil(Math.random() * 100);
+};
 
 // Build player template
+const view = (playerObj) => {
+  let player = document.createElement("div");
+  player.classList.add("player");
+  let image = document.createElement("img");
+  image.setAttribute("src", playerObj.image);
+  image.setAttribute("alt", "");
+  let name = document.createElement("div");
+  name.className = "name";
+  name.textContent = playerObj.name;
+  let strength = document.createElement("div");
+  strength.textContent = playerObj.strength;
+  strength.className = "strength";
+  player.append(image, name, strength);
+  return player;
+};
+
 const buildPlayers = (players, type) => {
-    let fragment = '';
+  let wrapper = document.createElement("div");
 
-    // Instead of using for loop
-    // Use chaining of Array methods - filter, map and join
-    // Type your code here
+  players
+    .filter((player) => player.type === type)
+    .forEach((player) => {
+      let playerView = view(player);
+      wrapper.appendChild(playerView);
+    });
 
-    return fragment;
-}
+  return wrapper.innerHTML;
+};
 
 // Display players in HTML
 const viewPlayers = (players) => {
-    document.getElementById('heroes').innerHTML = buildPlayers(players, 'hero');
-    document.getElementById('villains').innerHTML = buildPlayers(players, 'villain');
-}
-
+  document.getElementById("heroes").innerHTML = buildPlayers(players, "hero");
+  document.getElementById("villains").innerHTML = buildPlayers(
+    players,
+    "villain"
+  );
+};
 
 window.onload = () => {
-    viewPlayers(initPlayers(PLAYERS));
-}
+  viewPlayers(initPlayers(PLAYERS));
+};
